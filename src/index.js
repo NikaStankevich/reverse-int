8d83cc544086c04d1e394a858a5ef592ff98b5c3
Nika Stankevich <kladnickaya@gmail.com>
2022-08-10 22:22:33 +0300

    feat solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let numberToRevers = Math.abs(n);
+    
+    let stringFromNumber = numberToRevers.toString(10);
+    let reversedString = stringFromNumber.split('').reverse().join('');
+    let reversedNumber = parseInt(reversedString, 10);
+
+    return reversedNumber;
 }
